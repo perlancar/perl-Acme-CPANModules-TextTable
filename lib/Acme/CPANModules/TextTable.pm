11854a44979d9622ea0d5a03dbaac97f553b7165
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-01-11 09:29:49 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/TextTable.pm
@@ -0,0 +1,232 @@
+package Acme::CPANModules::TextTable;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub _make_table {
+    my ($cols, $rows) = @_;
+    my $res = [];
+    push @$res, [];
+    for (0..$cols-1) { $res->[0][$_] = "col" . ($_+1) }
+    for my $row (1..$rows) {
+        push @$res, [ map { "row$row.$_" } 1..$cols ];
+    }
+    $res;
+}
+
+our $LIST = {
+    summary => 'Modules that generate text tables',
+    entry_features => {
+        wide_char => {summary => 'Whether the use of wide characters (e.g. Kanji) in cells does not cause the table to be misaligned'},
+        color => {summary => 'Whether the module supports ANSI colors'},
+        box_char => {summary => 'Whether the module can utilize box-drawing characters'},
+    },
+    entries => [
+        {
+            module => 'Text::ANSITable',
+            bench_code => sub {
+                my ($table) = @_;
+                my $t = Text::ANSITable->new(
+                    use_utf8 => 0,
+                    use_box_chars => 0,
+                    use_color => 0,
+                    columns => $table->[0],
+                    border_style => 'Default::single_ascii',
+                );
+                $t->add_row($table->[$_]) for 1..@$table-1;
+                $t->draw;
+            },
+            features => {
+                wide_char => 1,
+                color => 1,
+                box_char => 1,
+            },
+        },
+        {
+            module => 'Text::ASCIITable',
+            bench_code => sub {
+                my ($table) = @_;
+                my $t = Text::ASCIITable->new();
+                $t->setCols(@{ $table->[0] });
+                $t->addRow(@{ $table->[$_] }) for 1..@$table-1;
+                "$t";
+            },
+            features => {
+                wide_char => 0,
+                color => 0,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::FormatTable',
+            bench_code => sub {
+                my ($table) = @_;
+                my $t = Text::FormatTable->new(join('|', ('l') x @{ $table->[0] }));
+                $t->head(@{ $table->[0] });
+                $t->row(@{ $table->[$_] }) for 1..@$table-1;
+                $t->render;
+            },
+            features => {
+                wide_char => 0,
+                color => 0,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::MarkdownTable',
+            bench_code => sub {
+                my ($table) = @_;
+                my $out = "";
+                my $t = Text::MarkdownTable->new(file => \$out);
+                my $fields = $table->[0];
+                foreach (1..@$table-1) {
+                    my $row = $table->[$_];
+                    $t->add( {
+                        map { $fields->[$_] => $row->[$_] } 0..@$fields-1
+                    });
+                }
+                $t->done;
+                $out;
+            },
+            features => {
+                wide_char => 0,
+                color => 0,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::Table',
+            bench_code => sub {
+                my ($table) = @_;
+                my $t = Text::Table->new(@{ $table->[0] });
+                $t->load(@{ $table }[1..@$table-1]);
+                $t;
+            },
+            features => {
+                wide_char => 0,
+                color => 0,
+                box_char => {value=>undef, summary=>'Does not draw borders'},
+            },
+        },
+        {
+            module => 'Text::Table::Tiny',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::Tiny::table(rows=>$table, header_row=>1);
+            },
+            features => {
+                wide_char => 0,
+                color => 0,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::Table::TinyColor',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::TinyColor::table(rows=>$table, header_row=>1);
+            },
+            features => {
+                wide_char => 0,
+                color => 1,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::Table::TinyColorWide',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::TinyColorWide::table(rows=>$table, header_row=>1);
+            },
+            features => {
+                wide_char => 1,
+                color => 1,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::Table::TinyWide',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::TinyWide::table(rows=>$table, header_row=>1);
+            },
+            features => {
+                wide_char => 1,
+                color => 0,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::Table::Org',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::Org::table(rows=>$table, header_row=>1);
+            },
+            features => {
+                wide_char => 0,
+                color => 0,
+                box_char => 0,
+            },
+        },
+        {
+            module => 'Text::Table::CSV',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::CSV::table(rows=>$table, header_row=>1);
+            },
+            features => {
+                wide_char => 1,
+                color => 0,
+                box_char => {value=>undef, summary=>"Irrelevant"},
+            },
+        },
+        {
+            module => 'Text::Table::HTML',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::HTML::table(rows=>$table, header_row=>1);
+            },
+            features => {
+            },
+        },
+        {
+            module => 'Text::Table::HTML::DataTables',
+            bench_code => sub {
+                my ($table) = @_;
+                Text::Table::HTML::DataTables::table(rows=>$table, header_row=>1);
+            },
+            features => {
+            },
+        },
+        {
+            module => 'Text::TabularDisplay',
+            bench_code => sub {
+                my ($table) = @_;
+                my $t = Text::TabularDisplay->new(@{ $table->[0] });
+                $t->add(@{ $table->[$_] }) for 1..@$table-1;
+                $t->render; # doesn't add newline
+            },
+            features => {
+                wide_char => 1,
+                color => 0,
+                box_char => {value=>undef, summary=>"Irrelevant"},
+            },
+        },
+    ],
+
+    bench_datasets => [
+        {name=>'tiny (1x1)'    , argv => [_make_table( 1, 1)],},
+        {name=>'small (3x5)'   , argv => [_make_table( 3, 5)],},
+        {name=>'wide (30x5)'   , argv => [_make_table(30, 5)],},
+        {name=>'long (3x300)'  , argv => [_make_table( 3, 300)],},
+        {name=>'large (30x300)', argv => [_make_table(30, 300)],},
+    ],
+
+};
+
+1;
+# ABSTRACT:
